1b3ca1bf90926be4ae06545ba4332d95e7d380d5
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-02-21 22:46:16 -0800

    gyronorm test

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 const background = document.querySelector('.background');
+//
+// function handleOrientation(event) {
+//   var absolute = event.absolute;
+//   var alpha    = event.alpha;
+//   let beta     = event.beta;
+//   var gamma    = event.gamma;
+//
+// 	if (beta<0){
+// 		beta = 0;
+// 	}
+// 	beta*=0.1;
+//
 
-function handleOrientation(event) {
-  var absolute = event.absolute;
-  var alpha    = event.alpha;
-  let beta     = event.beta;
-  var gamma    = event.gamma;
+// }
+// console.log('testing');
+// window.addEventListener("deviceorientation", handleOrientation, true);
 
-	if (beta<0){
-		beta = 0;
-	}
-	beta*=0.1;
+var gn = new GyroNorm();
+
+gn.init().then(function(){
+  gn.start(function(data){
+
+    const alpha = data.do.alpha;
+    const beta = data.do.beta;
 
 	background.style.transform = `rotateY(${alpha*0.2222}deg) rotateX(${(180-beta)}deg)`;
-}
-console.log('testing');
-window.addEventListener("deviceorientation", handleOrientation, true);
+
+  });
+}).catch(function(e){
+	console.log(e);
+});
