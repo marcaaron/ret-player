8067e19105d3152071bf3e4dec7c1d9b8bbd756d
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-02-21 22:55:23 -0800

    device orientation adjust

--- js/app.js
+++ js/app.js
@@ -25,8 +25,7 @@ gn.init().then(function(){
     // data.do.gamma	( deviceorientation event gamma value )
     // data.do.absolute	( deviceorientation event absolute value )
 
-	// background.style.transform = `rotateY(${data.do.gamma}deg)`;
-	background.style.transform = `	rotateX(${data.do.beta}deg)`;
+	background.style.transform = `rotateY(${data.do.gamma}deg) rotateX(${data.do.beta}deg)`;
 
 
   });
